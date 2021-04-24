1cc2a34512429919782b7eec188ff392beabd1e3
Ram Alvapillai <ramanan.a125@gmail.com>
2021-04-24 12:23:31 -0700

    passed down io instance to controller

--- routes/socketRoutes.js
+++ routes/socketRoutes.js
@@ -2,19 +2,18 @@ const chatController = require('../controllers/chat')
 const messageController = require('../controllers/message')
 const userController = require('../controllers/user')
 
-const getSocketRoutes = () => {
-  socket.on('getConvos', async (user, callback) =>  
+const getSocketRoutes = (socket, io) => {
+  socket.on('getConvos', async (user, callback) =>  {
     chatController.getConvos(user, callback)
-  )
+  })
 
   socket.on('getMessageHistory', async (value, callback) => 
     chatController.getMessageHistory(value, callback)
   )
 
   socket.on('sendMessage', async (data) => {
-    messageController.sendMessage(data);
+    messageController.sendMessage(data, io);
   })
-
 }
 
 module.exports = getSocketRoutes;
