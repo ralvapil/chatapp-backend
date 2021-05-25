029728a0d5a5cd23820ad3f0346151d6e5e3663e
Ram Alvapillai <ramanan.a125@gmail.com>
2021-05-25 00:31:43 -0700

    added linting

--- setupPassport.js
+++ setupPassport.js
@@ -1,28 +1,33 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const passport = require('passport')
+const passport = require('passport');
 const UserService = require('./services/UserService');
 
 const setupPassport = () => {
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser((user, done) => {
     console.log('serialize user', user);
     done(null, user.id);
   });
-  
-  passport.deserializeUser(async function(id, done) {
+
+  passport.deserializeUser(async (id, done) => {
     const user = await UserService.getUser(id);
     done(null, user);
   });
-  
-  passport.use(new GoogleStrategy({
-    clientID: '17046437582-pbhk4jniie6m0p7llcttm3r4770t6tv2.apps.googleusercontent.com',
-    clientSecret: 'fnTOqOhBNLB7wvsU5MkfpHj1',
-    callbackURL: "https://limitless-inlet-11254.herokuapp.com/auth/google/callback"
-  },
-    async function(accessToken, refreshToken, profile, done) {
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID:
+          '17046437582-pbhk4jniie6m0p7llcttm3r4770t6tv2.apps.googleusercontent.com',
+        clientSecret: 'fnTOqOhBNLB7wvsU5MkfpHj1',
+        callbackURL:
+          'https://limitless-inlet-11254.herokuapp.com/auth/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
         const user = await UserService.findByGoogleIdOrCreate(profile);
         return done(null, user);
-    }
-  ));
-}
+      }
+    )
+  );
+};
 
 module.exports = setupPassport;
